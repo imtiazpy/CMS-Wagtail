7f077cb0681e57e4ff4b97e6f8479af523b485a7
imtiazpy <ahmed.imti1994@gmail.com>
2024-09-01 12:19:22 +0600

    updated wagtail version, removed old migrations and added new ones, changed language switcher in header, added config for favicon in nginx.conf

--- app/stoiclife/settings/production.py
+++ app/stoiclife/settings/production.py
@@ -4,6 +4,7 @@ DEBUG = env.bool("DEBUG", default=False)
 SECRET_KEY = env.str("SECRET_KEY")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["http://localhost:1337"])
+CSRF_COOKIE_SECURE=env.bool("CSRF_COOKIE_SECURE", default=True)
 
 try:
     from .local import *
