dd2fe2bce16ce826bfcb998ecd7a522db9e5bd9f
Carlos Perez <perezcarlos8553@gmail.com>
2024-11-05 23:28:20 +0600

    added comment on production.py

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 
+# Keep debug false in production
 DEBUG = env.bool("DEBUG", default=False)
 SECRET_KEY = env.str("SECRET_KEY")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
